7545c50f8f50e3f551a2b0f3fb9990d3f0cfd40b
Richard Nemeth <nemethricsi@Richards-Private-MacBook-Pro.local>
2023-04-23 10:36:25 +0200

    style portable text

--- app/post/[postSlug]/page.tsx
+++ app/post/[postSlug]/page.tsx
@@ -1,5 +1,6 @@
 import { getPost } from '@/sanity/sanity-utils';
 import { PortableText } from '@portabletext/react';
+import type { PortableTextComponents } from '@portabletext/react';
 
 interface SinglePostProps {
   params: {
@@ -11,10 +12,32 @@ export default async function SinglePost({ params }: SinglePostProps) {
   const slug = params.postSlug;
   const { title, content } = await getPost(slug);
 
+  const components: PortableTextComponents = {
+    marks: {
+      link: ({ children, value }) => {
+        return (
+          <a
+            href={value.href}
+            className="text-blue-500 underline underline-offset-2"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {children}
+          </a>
+        );
+      },
+    },
+  };
+
   return (
     <>
-      <h1>{title}</h1>
-      <PortableText value={content} />
+      <h1 className="text-2xl md:text-4xl">{title}</h1>
+      <a href="https://www.feedthemogul.com">
+        Ez itt egy link kivul a portable text-en.
+      </a>
+      <div id="portable-text">
+        <PortableText value={content} components={components} />
+      </div>
     </>
   );
 }
